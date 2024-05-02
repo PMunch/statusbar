f9dd41c68a54aa1519ee3c6f55d074c5179307a9
PMunch <peterme@peterme.net>
2024-05-02 09:35:45 +0200

    Update volume script

--- examples/volume.sh
+++ examples/volume.sh
@@ -2,14 +2,11 @@
 # Simple script to create a block and update it with the current date and time
 # Typically run by the same keybinding which sets volume, and probably as cron
 # job with the pattern '* * * * *' to run it every minute just in case something
-# else changed the volume.
+# else changed the volume. NOTE: For cron to be able to use `pactl` you add
+# XDG_RUNTIME_DIR=/run/user/$(id -u) before your command.
 mountpath="${MOUNTPATH:-/tmp/statusbar}"
 order="${ORDER:-40}"
 mkdir -p "$mountpath/$order-volume"
-mute=$(pactl list sinks | awk '$1 ~ /Mute:/{if ($2 == "no") print "🔊"; else print "🔇"}')
-volume=$(pactl list sinks | awk '$1 ~ /Volume:/{print $5}')
+mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '$1 ~ /Mute:/{if ($2 == "no") print "🔊"; else print "🔇"}')
+volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '$1 ~ /Volume:/{print $5}')
 paste -d" " <(echo $mute) <(echo $volume) > "$mountpath/$order-volume/content"
-# Technically the mute and volume awk scripts could both be placed in one
-# statement and not require the paste. But this requires the order of 'pactl
-# list sinks' to always list mute status before volume, which I wasn't sure
-# would always be the case.
